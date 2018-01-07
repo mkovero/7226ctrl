0ef1bacfa909c78de98523461d17666b8681586d
Markus Kovero <mui@mui.fi>
2018-01-07 15:47:04 +0200

    Cleanup, rewrite based on Tuomas Kantola's work, tried actually compiling now.

--- main/main.ino
+++ main/main.ino
@@ -31,17 +31,7 @@ int tpspin = A0;
 // Internals
 int gear = 1;
 int prevgear = 1;
-const char* message = "unused";
-const char* pin = "nothing";
-unsigned int n2_rpmCount = 0;
-unsigned long n2_timeold;
-volatile byte n2_hrev;
-unsigned int n3_rpmCount = 0;
-unsigned long n3_timeold;
-volatile byte n3_hrev;
-unsigned int engine_rpmCount = 0;
-unsigned long engine_timeold;
-volatile byte engine_hrev;
+int *pin;
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 static unsigned long thisMicros = 0;
 static unsigned long lastMicros = 0;
@@ -50,7 +40,6 @@ static unsigned long lastMicros = 0;
 int tempSwitch = 22;
 int gdownSwitch = 23;
 int gupSwitch = 24;
-int tccSwitch = 25;
 
 // States
 int prevtempState = 0;
@@ -67,7 +56,9 @@ int greenState = 0;
 int yellowState = 0;
 
 // Shit delay
-int shiftdelay = 1000;
+int shiftDelay = 1000;
+int shiftStartTime = 0;
+int shiftDuration = 0;
 
 // Do we want torque lock?
 boolean tccEnabled = false; // no
@@ -77,6 +68,14 @@ boolean incar = false; // no.
 
 // Do we use stick control?
 boolean stick = true; // yes.
+
+// Manual control?
+boolean manual = true;
+
+// Actual transmission there?
+boolean trans = true;
+
+boolean switchBlocker = false;
 
 void setup() {
   
@@ -93,12 +92,6 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
  
-  // Interrupt for RPM read, check RISING/FALLING on live setup.
-  // We assume there are two FALLING edges per revolution.
-  attachInterrupt(0, n2_rpm, FALLING); // pin 2
-  attachInterrupt(1, n3_rpm, FALLING); // pin 3 
-  attachInterrupt(2, engine_rpm, FALLING); // pin 21
- 
   // Solenoid outputs
   pinMode(y3, OUTPUT); // 1-2/4-5 solenoid
   pinMode(y4,OUTPUT); // 2-3
@@ -110,7 +103,6 @@ void setup() {
   //For manual control
   pinMode(gupSwitch,INPUT); // gear up
   pinMode(gdownSwitch,INPUT); // gear down
-  pinMode(tccSwitch,INPUT); // torque lock
   
   //For stick control
   pinMode(whitepin,INPUT);
@@ -119,12 +111,12 @@ void setup() {
   pinMode(yellowpin,INPUT);
 
   // Drive solenoids are all off.
-  digitalWrite(y3,HIGH);
-  digitalWrite(y4,HIGH);
-  digitalWrite(y5,HIGH);
-  digitalWrite(spc,HIGH); // No pressure here by default.
-  digitalWrite(mpc,LOW); // We want constant pressure here.
-  digitalWrite(tcc,HIGH); // No pressure here by default.
+  analogWrite(y3,0);
+  analogWrite(y4,0);
+  analogWrite(y5,0);
+  analogWrite(spc,0); // No pressure here by default.
+  analogWrite(mpc,255); // We want constant pressure here.
+  analogWrite(tcc,0); // No pressure here by default.
 
   //Internals
   pinMode(tpspin,INPUT); // throttle position sensor
@@ -132,6 +124,11 @@ void setup() {
   Serial.println("Started.");
 }
 
+// UI STAGE
+// Control for what user sees and how gearbox is used with
+// 
+
+// Display update
 void updateDisplay() {
   display.clearDisplay();
   display.setCursor(0,0);
@@ -139,154 +136,134 @@ void updateDisplay() {
   display.display();
 }
 
-// Drive solenoid state change (replace delay() with something else that does not block everything.)
-void switchGear(const char* pin, const char* message) {
-   digitalWrite(spc,LOW); // We could change shift pressure here LOW = maxed out
-   digitalWrite(pin,HIGH); // Beginning of gear change
-   delay(shiftdelay);
-   digitalWrite(pin,LOW); // End of gear change
-   digitalWrite(spc,HIGH); // let go of shift pressure
-   Serial.println(message);
-}
-
-// For manual control, gear up
-void gearup() {
-  if (gear > 4) { gear = 5; } // Make sure not to switch more than 5.
-  prevgear = gear;
-  gear = gear+1;
-  gearchange(gear);
-}
-
-// For manual control, gear down
-void geardown() {
-  if (gear < 2) { gear = 1; } // Make sure not to switch less than 1.
-  prevgear = gear;
-  gear = gear-1;
-  gearchange(gear);
-}
-
-// General gear change solenoid logic
-gearchange(int gear) {
-  prevgear = gear; // Make sure previous gear is known
- 
-  switch (gear) {
-    case 1: 
-      if ( prevgear == 2 ) { switchGear(y3, "from 2->1"); gear = 1; }
-      break;
-    case 2:
-      if ( prevgear == 1 ) { switchGear(y3, "from 1->2"); gear = 2; }
-      if ( prevgear == 3 ) { switchGear(y5, "from 3->2"); gear = 2; }
-      break;
-    case 3:
-      if ( prevgear == 2 ) { switchGear(y5, "from 2->3"); gear = 3; }
-      if ( prevgear == 4 ) { switchGear(y4, "from 4->3"); gear = 3; }
-      break;
-    case 4:
-      if ( prevgear == 3 ) { switchGear(y4, "from 3->4"); gear = 4; }
-      if ( prevgear == 5 ) { switchGear(y3, "from 5->4"); gear = 4; }
-      break;
-    case 5:
-      if ( prevgear == 4 ) { switchGear(y3, "from 4->5"); gear = 5; }
-      break;
-    case 6:
-      // mechanical "N" gear
-      break;
-    case 7:
-      // mechanical "R" gear
-      break;
-    case 8:
-      // mechanical "P" gear
-      break;
-    default:
-    break;
-  }
-  updateDisplay();
-}
-
+// INPUT
 // Polling for stick control
-pollstick() {
+void pollstick() {
   // Read the stick.
   whiteState = digitalRead(whitepin);
   blueState = digitalRead(bluepin);
   greenState = digitalRead(greenpin);
   yellowState = digitalRead(yellowpin);
   // Determine position
-  if (whiteState == HIGH && blueState == HIGH && greenState == HIGH && yellowState == LOW ) { gearchange(8); } // P 
-  if (whiteState == LOW && blueState == HIGH && greenState == HIGH && yellowState == HIGH ) { gearchange(7); } // R
-  if (whiteState == HIGH && blueState == LOW && greenState == HIGH && yellowState == HIGH ) { gearchange(6); } // N
-  if (whiteState == LOW && blueState == LOW && greenState == HIGH && yellowState == LOW ) { gearchange(5); }
-  if (whiteState == LOW && blueState == LOW && greenState == LOW && yellowState == HIGH ) { gearchange(4); }
-  if (whiteState == LOW && blueState == HIGH && greenState == LOW && yellowState == LOW ) { gearchange(3); }
-  if (whiteState == HIGH && blueState == LOW && greenState == LOW && yellowState == LOW ) { gearchange(2); }
-  if (whiteState == HIGH && blueState == HIGH && greenState == LOW && yellowState == HIGH ) { gearchange(1); }
+  if (whiteState == HIGH && blueState == HIGH && greenState == HIGH && yellowState == LOW ) { gear = 8; } // P
+  if (whiteState == LOW && blueState == HIGH && greenState == HIGH && yellowState == HIGH ) { gear = 7; } // R
+  if (whiteState == HIGH && blueState == LOW && greenState == HIGH && yellowState == HIGH ) { gear = 6; } // N
+  if (whiteState == LOW && blueState == LOW && greenState == HIGH && yellowState == LOW ) { gear = 5; }
+  if (whiteState == LOW && blueState == LOW && greenState == LOW && yellowState == HIGH ) { gear = 4; }
+  if (whiteState == LOW && blueState == HIGH && greenState == LOW && yellowState == LOW ) { gear = 3; }
+  if (whiteState == HIGH && blueState == LOW && greenState == LOW && yellowState == LOW ) { gear = 2; }
+  if (whiteState == HIGH && blueState == HIGH && greenState == LOW && yellowState == HIGH ) { gear = 1; }
+  gearchange();
+}
+// For manual control, gear up
+void gearup() {
+  prevgear = gear;
+  gear = gear+1;
+  if (gear > 4) { gear = 5; } // Make sure not to switch more than 5.
+  if ( ! prevgear == gear) { gearchange(); }
+}
+
+// For manual control, gear down
+void geardown() {
+  prevgear = gear;
+  gear = gear-1;
+  if (gear < 2) { gear = 1; } // Make sure not to switch less than 1.
+  if ( ! prevgear == gear) { gearchange(); }
 }
 
 // Polling for manual switch keys
-pollkeys() {
+void pollkeys() {
   gupState == digitalRead(gupSwitch); // Gear up
   gdownState == digitalRead(gdownSwitch); // Gear down
-  
+
   if (gdownState != prevgdownState || gupState != prevgupState ) {
     if (gdownState == LOW && gupState == HIGH) {
       prevgupState = gupState;
-      gearup(); 
+      gearup();
     } else if (gupState == LOW && gdownState == HIGH) {
       prevgdownState = gdownState;
       geardown();
     }
   }
 }
-void rpmcheck() {
- //Update RPM every 20 counts, increase this for better RPM resolution,
- //decrease for faster update
- // For engine RPM
- if (engine_hrev >= 20) {
-     detachInterrupt(2); //Disable interrupt when calculating
-     engine_rpm = 30*1000/(millis() - engine_timeold)*engine_hrev;
-     engine_timeold = millis();
-     engine_hrev = 0;
-     attachInterrupt(2, engine_rpm, FALLING); //enable interrupt
- }
- // For N2
- if (n2_hrev >= 20) { 
-     detachInterrupt(0); //Disable interrupt when calculating
-     engine_rpm = 30*1000/(millis() - n2_timeold)*n2_hrev;
-     n2_timeold = millis();
-     n2_hrev = 0;
-     attachInterrupt(0, n2_rpm, FALLING); //enable interrupt
- }
- // For N3
- if (n3_hrev >= 20) {
-     detachInterrupt(1); //Disable interrupt when calculating
-     engine_rpm = 30*1000/(millis() - n3_timeold)*n3_hrev;
-     n3_timeold = millis();
-     n3_hrev = 0;
-     attachInterrupt(1, n3_rpm, FALLING); //enable interrupt
- }
+
+// END OF UI STAGE / INPUT
+
+// CORE
+// no pressure alteration happening yet
+//  
+// gearSwitch logic
+void switchGear() {
+   switchBlocker = true;
+   switchGearStart();
 }
+
+void switchGearStart() {
+   analogWrite(spc,255); // We could change shift pressure here 
+   analogWrite(*pin,255); // Beginning of gear change
+}
+
+void switchGearStop() {
+   analogWrite(*pin,0); // End of gear change
+   analogWrite(spc,0); // let go of shift pressure
+   switchBlocker = false;
+}
+
+void polltrans() {
+   shiftDuration = millis() - shiftStartTime;
+   if ( shiftDuration > shiftDelay) { switchGearStop(); };
+}
+
+// Solenoid logic
+void gearchange() {
+  prevgear = gear; // Make sure previous gear is known
+  shiftStartTime = millis(); 
+  if ( switchBlocker == false ) { 
+    switch (gear) {
+      case 1: 
+        if ( prevgear == 2 ) { pin = &y3; switchGear(); gear = 1; };
+        break;
+      case 2:
+        if ( prevgear == 1 ) { pin = &y3; switchGear(); gear = 2; };
+        if ( prevgear == 3 ) { pin = &y5; switchGear(); gear = 2; };
+        break;
+      case 3:
+        if ( prevgear == 2 ) { pin = &y5; switchGear(); gear = 3; };
+        if ( prevgear == 4 ) { pin = &y4; switchGear(); gear = 3; };
+        break;
+      case 4:
+        if ( prevgear == 3 ) { pin = &y4; switchGear(); gear = 4; };
+        if ( prevgear == 5 ) { pin = &y3; switchGear(); gear = 4; };
+        break;
+      case 5:
+        if ( prevgear == 4 ) { pin = &y3; switchGear(); gear = 5; };
+        break;
+      case 6:
+        // mechanical "N" gear
+        break;
+      case 7:
+        // mechanical "R" gear
+        break;
+      case 8:
+        // mechanical "P" gear
+        break;
+      default:
+      break;
+    }
+  }
+  updateDisplay();
+}
+// END OF CORE
+
 
 void loop() {
   // Get temperature
   tempState = digitalRead(tempSwitch);
   // If we have a temperature, we can assume P/N switch has moved to R/N. (Lever switch and temp sensor are in series)
   if ( tempState == HIGH ) {
-    if ( stick == true) { pollstick(); } // using stick
-    if ( manual == true) { pollkeys(); } // using manual control
-    if ( tccEnabled == true { tccState = digitalRead(tccSwitch); } // Torque lock requested 
+    if ( stick == true ) { pollstick(); } // using stick
+    if ( manual == true ) { pollkeys(); } // using manual control
+    if ( trans == true ) { polltrans(); } // using transmission
+
   }
-  rpmcheck();
 }
-
-// Interrupt value increment when hit
-void engine_rpm() {
-  engine_hrev++;
-}
-
-void n2_rpm() {
-  n2_hrev++;
-}
-
-void n3_rpm() {
-  n3_hrev++;
-}
-
