81e33f052cb15984d0916580a4a2b35deb61c016
Markus Kovero <mui@mui.fi>
2018-09-12 15:51:26 +0300

    faster refresh on display

--- main/main.ino
+++ main/main.ino
@@ -34,7 +34,7 @@
 #include <AutoPID.h>
 
 // "Protothreading", we have time slots for different functions to be run.
-Task pollDisplay(1000, updateDisplay);    // 500ms to update display*/
+Task pollDisplay(200, updateDisplay);    // 500ms to update display*/
 Task pollData(200, datalog);              // 200ms to update datalogging
 Task pollStick(100, pollstick);           // 100ms for checking stick position*
 Task pollGear(200, decideGear);           // 200ms for deciding new gear*/
