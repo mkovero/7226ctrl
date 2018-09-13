342d06c4456c7d7528c75cf82f118c3a05c5ba2d
Markus <markus@Markuss-MacBook-Pro-2.local>
2018-09-13 20:03:29 +0300

    tcc tuning, new page for buttons

--- main/input.cpp
+++ main/input.cpp
@@ -49,7 +49,7 @@ void pollstick(Task *me)
     gear = 2; // force reset gear to 2
     shiftPending = false;
     shiftBlocker = false;
-  }           // P
+  } // P
   if (whiteState == LOW && blueState == HIGH && greenState == HIGH && yellowState == HIGH)
   {
     wantedGear = 7;
@@ -228,8 +228,8 @@ void boostControl(Task *me)
     if (sensor.curBoostLim > 0)
     {
       myPID.run();
-   //   analogWrite(boostCtrl, boostPWM);
- //  if (debugEnabled) { Serial.print("BoostPWM = "); Serial.println(boostPWM); }
+      //   analogWrite(boostCtrl, boostPWM);
+      //  if (debugEnabled) { Serial.print("BoostPWM = "); Serial.println(boostPWM); }
     }
     else
     {
@@ -320,10 +320,10 @@ void polltrans(Task *me)
     if (wantedGear <= 6 || wantedGear == 8)
     {
       int mpcSetVal = (100 - mpcVal) * 2.55;
-     // analogWrite(mpc, mpcSetVal);
+      // analogWrite(mpc, mpcSetVal);
     }
 
-    if ((wantedGear == 7 || (wantedGear < 6 && !shiftPending)) && garageShift && (millis() - garageTime > 3000))
+    if ((wantedGear == 7 || (wantedGear < 6 && !shiftPending)) && garageShift && (millis() - garageTime > 1000))
     {
       analogWrite(spc, 0);
       garageShift = false;
@@ -334,15 +334,20 @@ void polltrans(Task *me)
     {
       analogWrite(y5, 255);
     }
-    else if (!shiftBlocker)
+    else if (!shiftBlocker && wantedGear < 6)
     {
       analogWrite(y5, 0);
     }
-    if (sensor.curTps < 30 && sensor.curSpeed > 80 && gear == 5) {
+
+    if (sensor.curTps < 40 && sensor.curSpeed > 80 && gear == 5)
+    {
       analogWrite(tcc, 255);
-    } else {
+    }
+    else
+    {
       analogWrite(tcc, 0);
     }
+
     // "1-2/4-5 Solenoid is pulsed during ignition crank." stop doing this after we get ourselves together.
     if (ignition)
     {
@@ -466,25 +471,25 @@ void radioControl()
     }
     else if (readData == 150)
     {
-      if (page == 1)
+      if (page < 3)
       {
         page++;
       }
-      else if (page == 2)
+      else if (page == 3)
       {
-        page--;
+        page == 1;
       }
       readData = 0;
     }
     else if (readData == 151)
     {
-      if (page == 2)
+      if (page > 1)
       {
         page--;
       }
       else if (page == 1)
       {
-        page++;
+        page == 3;
       }
       readData = 0;
     }
