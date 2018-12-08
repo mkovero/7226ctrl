0c6cc52c0c6ca4723ed7404c7ec2dbbb7e5a85a4
Markus <markus@Markuss-MacBook-Pro-2.local>
2018-12-08 20:00:54 +0200

    first version of map interpolation

--- main/calc.cpp
+++ main/calc.cpp
@@ -10,26 +10,24 @@ int maxBoostPressure = 700; // Max pressure on boost sensor
 // Mapping throttle position sensor voltage to percentage
 int readTPSVoltage(int voltage)
 {
- 
+
   int result = map(voltage, 1900, 800, 0, 100);
-    return result;
-  
+  return result;
 }
 
 // Mapping boost sensor voltage to percentage
 int readBoostVoltage(int voltage)
 {
- 
-  int result = map(voltage, 680, 2670, 0, 3000); // NXP MPX5700AP (range 0-700kPa)
-    return result;
 
+  int result = map(voltage, 680, 2670, 0, 3000); // NXP MPX5700AP (range 0-700kPa)
+  return result;
 }
 // Mapping battery voltage to actual voltage
 int readBatVoltage(int voltage)
 {
- 
+
   int result = map(voltage, 0, 3375, 0, 15000); // 0-15V
-    return result;
+  return result;
 }
 
 // Function to read 2d maps from flash (maps declared with PROGMEM)
@@ -64,9 +62,85 @@ int readMap(const int theMap[14][12], int x, int y)
   lastXval = xidx;
   lastYval = yidx;
   int mapValue = pgm_read_word_near(&theMap[yidx][xidx]);
-  
+
   return mapValue;
 }
+
+int readPercentualMap(const int theMap[14][12], int x, int y)
+{
+  int xidx = 0; // by default near first element
+  int xelements = LEN(theMap[0]);
+
+  int distance = abs(pgm_read_word_near(&theMap[0][xidx]) - x); // distance comparison
+  for (int i = 1; i < xelements; i++)
+  {
+    int d = abs(pgm_read_word_near(&theMap[0][i]) - x);
+    if (d < distance)
+    {
+      xidx = i;
+      distance = d;
+    }
+  }
+  int yidx = 0; // by default near first element
+  int yelements = LEN(*theMap);
+
+  distance = abs(pgm_read_word_near(&theMap[yidx][0]) - y);
+  for (int i = 1; i < yelements; i++)
+  {
+    int d = abs(pgm_read_word_near(&theMap[i][0]) - y);
+    if (d < distance)
+    {
+      yidx = i;
+      distance = d;
+    }
+  }
+  lastXval = xidx;
+  lastYval = yidx;
+  
+  int calculatedPoint = 0;
+  int mapValue = pgm_read_word_near(&theMap[yidx][xidx]);
+  int prevMapValue = pgm_read_word_near(&theMap[yidx - 1][xidx]);
+  int nextMapValue = pgm_read_word_near(&theMap[yidx + 1][xidx]);
+  int fuzzyMapValue = pgm_read_word_near(&theMap[yidx + 1][xidx - 1]);
+  int fuzzyMapValue2 = pgm_read_word_near(&theMap[yidx - 1][xidx - 1]);
+  int curY = pgm_read_word_near(&theMap[yidx][0]);
+  int curX = pgm_read_word_near(&theMap[0][xidx]);
+  int prevY = pgm_read_word_near(&theMap[yidx - 1][0]);
+  int prevX = pgm_read_word_near(&theMap[0][xidx - 1]);
+
+float betweenL1 = (curX - x) / (curX - prevX);
+float calculatedLine1 = mapValue - (betweenL1 * (mapValue - prevMapValue));
+
+if ( x > curX ) {
+    float calculatedLine2 = nextMapValue - (betweenL1 * (nextMapValue - fuzzyMapValue));
+    float twoPoints = ((y - curY) / prevY - curY);
+    float calculatedPoint = calculatedLine2 - (twoPoints * (calculatedLine1 - calculatedLine2));
+}
+if ( x < curX ) {
+    float calculatedLine2 = prevMapValue - (betweenL1 * (prevMapValue - fuzzyMapValue2));
+    float twoPoints = ((y - curY) / curY - prevY);
+    float calculatedPoint = calculatedLine2 - (twoPoints * (calculatedLine1 - calculatedLine2));
+}
+else {
+    float calculatedLine2 = calculatedLine1;
+    float twoPoints = ((y - curY) / prevY - curY);
+    float calculatedPoint = calculatedLine2 - (twoPoints * (calculatedLine1 - calculatedLine2));
+}
+
+//float percY = mapValue - (pgm_read_word_near(&theMap[yidx][0]) - y) / (pgm_read_word_near(&theMap[yidx][0]) - pgm_read_word_near(&theMap[yidx - 1][0])) * (mapValue * pgm_read_word_near(&theMap[yidx -1][xidx]));
+//  float percX = mapValue - (pgm_read_word_near(&theMap[0][xidx]) - x) / (pgm_read_word_near(&theMap[0][xidx]) - pgm_read_word_near(&theMap[0][xidx - 1])) * (mapValue * pgm_read_word_near(&theMap[yidx][xidx -1]));
+  
+  return calculatedPoint;
+}
+
+//35 / 40 = 0,875
+//40 / 35 = 1,142
+// 82-(40-35)/(40-30)*(82-72)
+// 35 = mitattu arvo
+// 40 = lähin y-akseli
+// 30 = aikaisempi y-akseli
+// 72 = aikaisemman arvo
+// 82 lähimmän arvo
 
 // Function to read 2d maps from flash (maps declared with PROGMEM)
 int readGearMap(const int theMap[14][6], int x, int y)
@@ -85,6 +159,6 @@ int readGearMap(const int theMap[14][6], int x, int y)
     }
   }
   int mapValue = pgm_read_word_near(&theMap[yidx][x]);
-  
+
   return mapValue;
 }
