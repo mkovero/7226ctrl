9ff62c8e107a74b2f5aa4d59f62cd9b7b45b06f9
Jami Karvanen <jami@karvanen.fi>
2018-12-19 21:54:39 +0200

    Add support for serial data (#11)

---
+++ dataviz/backend/index_serial.js
@@ -0,0 +1,36 @@
+const SerialPort = require('serialport');
+const WebSocket = require('ws');
+
+
+const wss = new WebSocket.Server({ port: 8080 });
+var ws;
+
+var serial = new SerialPort('/dev/ttyACM0', {
+  baudRate: 115200
+});
+
+
+wss.on('connection', function connection(ws_) {
+  console.log('New connection!');
+  ws_.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+  ws = ws_;
+});
+  
+serial.open(() => {
+  console.log("Opening serial")
+  let buffer = "";
+  serial.on('data', (data) => {
+    buffer += String(data);
+    while (buffer.match(/\r\n/)) {
+      match = buffer.match(/\r\n/);
+      let values = buffer.slice(0, match.index).split(';').map(val => parseFloat(val));
+      buffer = buffer.slice(match.index + 2);
+      console.log(values);
+      if (ws && ws.readyState === 1 && values.length === 17) {
+        ws.send(JSON.stringify(values));
+      }
+    }
+  });
+});
