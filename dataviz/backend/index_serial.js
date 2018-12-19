c44051276d4a540697c5de280053e213b9b1b0fb
Jami Karvanen <jami@karvanen.fi>
2018-12-19 23:24:38 +0200

    Fix serial data

--- dataviz/backend/index_serial.js
+++ dataviz/backend/index_serial.js
@@ -17,7 +17,27 @@ wss.on('connection', function connection(ws_) {
   });
   ws = ws_;
 });
-  
+
+function stripData(line) {
+    let newLine = []
+    let indexArray = [0, 1, 2, 4, 5, 6, 7, 8, 16]
+    indexArray.forEach(idx => newLine.push(line[idx]))
+    return newLine
+}
+
+function scaleData(line) {
+    let scaleFactor = 3000;
+    line[0] = (line[0]/300) * scaleFactor
+    line[1] = (line[1]/10/650) * scaleFactor
+    line[2] = (line[2]/100) * scaleFactor
+    line[3] = (line[3]/255) * scaleFactor
+    line[4] = (line[4]/255) * scaleFactor 
+    line[5] = (line[5]/100) * scaleFactor
+    line[6] = (line[6]/700) * scaleFactor 
+    line[7] = (line[7]/700) * scaleFactor 
+    line[8] = (line[8]/100/160) * scaleFactor 
+}
+
 serial.open(() => {
   console.log("Opening serial")
   let buffer = "";
@@ -27,8 +47,10 @@ serial.open(() => {
       match = buffer.match(/\r\n/);
       let values = buffer.slice(0, match.index).split(';').map(val => parseFloat(val));
       buffer = buffer.slice(match.index + 2);
+      values = stripData(values)
+      scaleData(values)
       console.log(values);
-      if (ws && ws.readyState === 1 && values.length === 17) {
+      if (ws && ws.readyState === 1 && values.length === 9) {
         ws.send(JSON.stringify(values));
       }
     }
