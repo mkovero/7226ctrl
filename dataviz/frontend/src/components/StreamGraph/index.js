8bf80d1b1d7c8bd5fc8ab7837ee290037e4090e1
Jami Karvanen <jami@karvanen.fi>
2018-12-19 21:26:23 +0200

    switch to 720p

--- dataviz/frontend/src/components/StreamGraph/index.js
+++ dataviz/frontend/src/components/StreamGraph/index.js
@@ -104,7 +104,7 @@ export default class StreamGraph extends Component {
     return (
       <div>
         <button onClick={() => console.log(data)}>Print data</button>
-        <svg id="chart" width="1080" height="1080"></svg>
+        <svg id="chart" width="720" height="720"></svg>
       </div>
     );
   }
