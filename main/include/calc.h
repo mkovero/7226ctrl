7f3a0a4d4c105dc84594817148de6f6e5ed81cd7
Markus <markus@Markuss-MacBook-Pro-2.local>
2019-01-26 13:19:39 +0200

    inverted temp maps support

--- main/include/calc.h
+++ main/include/calc.h
@@ -3,5 +3,6 @@ int readBoostVoltage(int voltage);
 int readBatVoltage(int voltage);
 int readMap(const int theMap[14][12], int x, int y);
 int readTempMap(const int theMap[23][2], int y);
+int readTempMapInverted(const int theMap[14][2], int y)
 int readPercentualMap(const int theMap[14][12], int x, int y);
 int readGearMap(const int theMap[14][6], int x, int y);
